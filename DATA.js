97ba2d4e5a5eb4f7a5806f4f8ffa54486059ca45
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-15 01:58:28 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "ee2e2e57-10b5-4307-93f2-01a11bcaa17d",
+    "home_feed": [
+        {
+            "id": "e0f2bc0b-0636-458e-a32b-1a7279b84722",
+            "artist_name": "crime",
+            "home_carousel": [
+                {
+                    "id": "b67f88d8-cb2f-428b-aa05-0f3d96f0718c",
+                    "art_photo": "https://images.unsplash.com/photo-1544091523-bbf51357b965?w=1080"
+                },
+                {
+                    "id": "7a0aa5da-dc57-459a-abca-24cca6d036b7",
+                    "art_photo": "https://images.unsplash.com/photo-1646576646021-d43c4320ce50?w=1080"
+                },
+                {
+                    "id": "5f112771-9670-4dca-bfc7-9ecdcb7a80ca",
+                    "art_photo": "https://images.unsplash.com/photo-1610826128389-ae3e0f31e81d?w=1080"
+                },
+                {
+                    "id": "3fec6d14-a815-43c3-aacd-04bf23e13244",
+                    "art_photo": "https://images.unsplash.com/photo-1610887535272-767df13b0fe4?w=1080"
+                },
+                {
+                    "id": "74aef96b-da55-4580-880b-fe0f8ec9e1ca",
+                    "art_photo": "https://images.unsplash.com/photo-1609854434952-752edd7b910d?w=1080"
+                }
+            ],
+            "art_caption": "for",
+            "art_description": "Teacher between box magazine. Performance maintain student while garden memory. Born often morning may."
+        },
+        {
+            "id": "c2596f79-6a83-4b3a-ae8b-60e1a1386736",
+            "artist_name": "always",
+            "home_carousel": [
+                {
+                    "id": "70b90217-e4b5-4fa2-ab55-8cfb9602e013",
+                    "art_photo": "https://images.unsplash.com/photo-1568482224714-7843360d588a?w=1080"
+                },
+                {
+                    "id": "de59a664-b6b5-4e38-959e-bb5290c9d112",
+                    "art_photo": "https://images.unsplash.com/photo-1604360360620-a650893fbc68?w=1080"
+                },
+                {
+                    "id": "ffbf3df7-edfd-4edd-b820-cbf720cba025",
+                    "art_photo": "https://images.unsplash.com/photo-1651315203597-4b0c428b1213?w=1080"
+                },
+                {
+                    "id": "bb4434aa-0bfa-49a4-9c8e-9f0d2b5f7920",
+                    "art_photo": "https://images.unsplash.com/photo-1480057261736-36852db40e50?w=1080"
+                },
+                {
+                    "id": "f8ea9a8e-1ee6-42f4-b225-895b68f85392",
+                    "art_photo": "https://images.unsplash.com/photo-1526820687106-ac87a50476d3?w=1080"
+                }
+            ],
+            "art_caption": "check",
+            "art_description": "Security election daughter. Land tree everything benefit concern. Easy consider evidence try hundred of."
+        },
+        {
+            "id": "402b7f69-d7c6-4240-8acc-a9466708b2e0",
+            "artist_name": "throw",
+            "home_carousel": [
+                {
+                    "id": "ab8c7e8e-fc45-4d32-891f-6466f4b9574d",
+                    "art_photo": "https://images.unsplash.com/photo-1509719482242-cb6d79f11801?w=1080"
+                },
+                {
+                    "id": "a94b1763-3c2d-4fdb-9d5c-a565f9d6925c",
+                    "art_photo": "https://images.unsplash.com/photo-1587813369210-4ce93b19bcfe?w=1080"
+                },
+                {
+                    "id": "6f4f5f46-2781-42ff-be38-1485da642f5b",
+                    "art_photo": "https://images.unsplash.com/photo-1634055740573-d01a8fea7555?w=1080"
+                },
+                {
+                    "id": "46ca0c8f-a6e5-4f7e-bdcd-bf0497b34083",
+                    "art_photo": "https://images.unsplash.com/photo-1598519502841-c56d7052056e?w=1080"
+                },
+                {
+                    "id": "812c8b18-2a7b-4313-873f-7ff888b88339",
+                    "art_photo": "https://images.unsplash.com/photo-1557040095-c2fadbde5d98?w=1080"
+                }
+            ],
+            "art_caption": "truth",
+            "art_description": "Large dream nature old attention bar. Business just oil respond pretty establish world."
+        },
+        {
+            "id": "4dbffc37-248a-4e6f-89ef-7ae38254bf2b",
+            "artist_name": "always",
+            "home_carousel": [
+                {
+                    "id": "572e82e6-d2e5-459d-9fac-1d7c615b50ae",
+                    "art_photo": "https://images.unsplash.com/photo-1592089875777-49a2985e6e38?w=1080"
+                },
+                {
+                    "id": "9c5f07c0-2995-424e-963b-2ad1744eb7c8",
+                    "art_photo": "https://images.unsplash.com/photo-1530021356476-0a6375ffe73b?w=1080"
+                },
+                {
+                    "id": "f519f4ab-7a62-4cb3-af26-e8ac0d071cb7",
+                    "art_photo": "https://images.unsplash.com/photo-1625631246362-62c2b65e1894?w=1080"
+                },
+                {
+                    "id": "ae2be3ed-6cbf-4c5c-84eb-f2ed51dd394c",
+                    "art_photo": "https://images.unsplash.com/photo-1591450144041-148115aee985?w=1080"
+                },
+                {
+                    "id": "4bfb8452-72f5-4b80-b0de-67efb17a6035",
+                    "art_photo": "https://images.unsplash.com/photo-1534332283000-444ae2be38c9?w=1080"
+                }
+            ],
+            "art_caption": "little",
+            "art_description": "Sister old all no interesting. Professional whatever particularly sometimes already."
+        },
+        {
+            "id": "2b1be957-33eb-4db1-8d3c-6a9238b1eadb",
+            "artist_name": "management",
+            "home_carousel": [
+                {
+                    "id": "1f7e552a-ba72-48c4-88b8-1e2d1fb2cd3c",
+                    "art_photo": "https://images.unsplash.com/photo-1547782126-87bb2bead14e?w=1080"
+                },
+                {
+                    "id": "a84d9935-fe6b-420b-975e-0ccbd2e29243",
+                    "art_photo": "https://images.unsplash.com/photo-1515586175273-a732c6625a51?w=1080"
+                },
+                {
+                    "id": "33bdc33b-1ceb-40c0-b2cd-4c6f01c61a63",
+                    "art_photo": "https://images.unsplash.com/photo-1542644384-49f9febd8443?w=1080"
+                },
+                {
+                    "id": "e6b56888-5015-4487-bf91-c40a0b5efe8e",
+                    "art_photo": "https://images.unsplash.com/photo-1648979471399-8c163aaa85b6?w=1080"
+                },
+                {
+                    "id": "7587a489-5eef-416a-9fe7-5b51dd47494d",
+                    "art_photo": "https://images.unsplash.com/photo-1489269637500-aa0e75768394?w=1080"
+                }
+            ],
+            "art_caption": "member",
+            "art_description": "Become trade great mean. Environmental plan myself avoid brother one."
+        }
+    ]
+}
